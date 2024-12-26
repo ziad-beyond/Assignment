2d6b07d331d9744793a6e5f5f3a70de4d134c91e
ziad-beyond <Z.alsaedi@beyondcompany.sa>
2024-12-26 16:21:11 +0300

    Edit

--- assignment8/src/app/not-found.js
+++ assignment8/src/app/not-found.js
@@ -12,7 +12,7 @@ export default function NotFound() {
                         href="/"
                         className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4"
                     >
-                        Back to Homepage
+                        Back to Home Page
                     </a>
                 </div>
             </div>
